6d32795028be5e2c5225a4f2a31a137b085fa719
obovoid <git.obovoid@gmail.com>
2023-10-09 18:39:36 +0200

    Created Electron Environment

---
+++ main.js
@@ -0,0 +1,29 @@
+
+const {app, BrowserWindow} = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    // MAC OS Support
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
